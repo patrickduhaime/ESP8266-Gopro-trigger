4aca41857f04faddb0eca7ecfff7628d095c817d
patrickduhaime <32279890+patrickduhaime@users.noreply.github.com>
2019-06-14 18:51:07 -0400

    Update esp8266.c

--- esp8266.c
+++ esp8266.c
@@ -1,9 +1,9 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
  
-  const char* gopro_ssid = "GP24588273"; //Your Wifi name (SSID)
-  const char* gopro_password = "goprohero"; //Your WiFi password
- 
+const char* gopro_ssid = "GP24588273"; //Your Wifi name (SSID)
+const char* gopro_password = "goprohero"; //Your WiFi password
+
 //Do not modify below this line//
 const char* gopro_host = "10.5.5.9"; //GoPro Hero 4 IP Address
  
@@ -16,26 +16,26 @@ int triggerState = 0;
 
 void setup()
 {
-  //Start communication to serial port
-  Serial.begin(115200);
-  delay(100);
-  pinMode(triggerPin, INPUT_PULLUP);
- 
-  //Wifi Connect to GoPro
-  Serial.print("Connecting to ");
-  Serial.println(gopro_ssid);
-  WiFi.begin(gopro_ssid, gopro_password);
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected to GoPro");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+	//Start communication to serial port
+	Serial.begin(115200);
+	delay(100);
+	pinMode(triggerPin, INPUT_PULLUP);
+
+	//Wifi Connect to GoPro
+	Serial.print("Connecting to ");
+	Serial.println(gopro_ssid);
+	WiFi.begin(gopro_ssid, gopro_password);
+	while (WiFi.status() != WL_CONNECTED)
+	{
+	delay(500);
+	Serial.print(".");
+	}
+	Serial.println("");
+	Serial.println("WiFi connected to GoPro");
+	Serial.println("IP address: ");
+	Serial.println(WiFi.localIP());
   
-  //put gopro into appropriate photo mode
+	//put gopro into appropriate photo mode
   
 	//Primary mode: PHOTO
 	RequestGoProURL("/gp/gpControl/command/mode?p=1");
@@ -51,18 +51,18 @@ void setup()
  
 void RequestGoProURL(String GoProURL)
 {
-  WiFiClient client;
-  const int httpPort = 80;
-  if (!client.connect(gopro_host, httpPort))
-  {
-    Serial.println("connection failed");
-    return;
-  }
-  //String url = "/gp/gpControl/command/mode?p=1";
-  client.print(String("GET ") + GoProURL + " HTTP/1.1\r\n" +
-  "Host: " + gopro_host + "\r\n" +
-  "Connection to Gopro: closed\r\n\r\n");
-  return;
+	WiFiClient client;
+	const int httpPort = 80;
+	if (!client.connect(gopro_host, httpPort))
+	{
+		Serial.println("connection failed");
+		return;
+	}
+	//String url = "/gp/gpControl/command/mode?p=1";
+	client.print(String("GET ") + GoProURL + " HTTP/1.1\r\n" +
+	"Host: " + gopro_host + "\r\n" +
+	"Connection to Gopro: closed\r\n\r\n");
+	return;
 }
  
  
@@ -83,4 +83,4 @@ void loop()
 		triggerState = 0;
 		pressTimer = 0;
 	}
- }
+}
